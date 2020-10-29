063e929db1028327718a07446760085e71bd52c4
Ahnaf Tahmid <ahnaf-tahmid@outlook.com>
2020-10-29 20:30:01 +0600

    class added

---
+++ micropython/geiger.py
@@ -0,0 +1,35 @@
+from machine import Pin, PWM
+from utime import sleep_ms
+
+@micropython.native
+class GMtube:
+    count = 0
+    def __init__(self,pwm=12,freq=10000,duty=675,event=15):
+        self.freq=freq
+        self.duty=duty
+        
+        #print('HV pin 12, freq 10kHz, duty 66%)
+        self.pwmpin = PWM(Pin(pwm), freq=freq, duty=duty)
+        #event pin 15
+        self.discharge = Pin(event, Pin.IN)
+        self.discharge.irq(trigger=Pin.IRQ_FALLING, handler= self.handle)
+
+        # Function to be called when the interrupt triggers
+    def handle(self,p):
+        global count
+        self.count+=1
+
+class Buzzer:
+    def __init__(self,pin1=2,pin2=4):
+        self.pin_1 = Pin(pin1, Pin.OUT)
+        self.pin_2 = Pin(pin2, Pin.OUT)
+        self.buzzer()
+    def buzzer(self):
+        self.pin_1.on()
+        self.pin_2.off()
+        # this goes into dead time of the counter
+        sleep_ms(2)
+        self.pin_1.off()
+        self.pin_2.on()
+        sleep_ms(2) 
+        self.pin_2.off()
