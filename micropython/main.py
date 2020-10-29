cfddaf7c07abb4a1d4f8c3ab3d27b5ca6377482f
Ahnaf Tahmid <ahnaf-tahmid@outlook.com>
2020-10-29 20:39:52 +0600

    removed Buzzer and Geiger and geiger added

--- micropython/main.py
+++ micropython/main.py
@@ -1,5 +1,4 @@
-import Geiger
-import Buzzer
+from geiger import GMtube,Buzzer
 from utime import *
 from umqtt.robust import MQTTClient
 from wlan_connect import auto_connect
@@ -9,20 +8,24 @@ auto_connect()
 SERVER = "192.168.1.100"        #IP or DNS record
 
 mqtt = MQTTClient("GM",SERVER)
-mqtt.connect()
 
-Geiger.pwm_400v()
-Geiger.discharge()
+try:
+    mqtt.connect()
+except:
+    print('mqtt connect failed')
+
+g=GMtube(12,10000,675,15)
+
 
 last_tick = ticks_ms()
 current_tick = ticks_ms()
 CPM = []
-count = 0
+event = 0
 
 while True:
     # check if a new event happened
-    if Geiger.count > count:
-        count = Geiger.count
+    if g.count > event:
+        event = g.count
         current_tick = ticks_ms()
         delta_t = ticks_diff(current_tick, last_tick)
         last_tick = current_tick
@@ -32,10 +35,12 @@ while True:
             while sum(CPM) > 60000:
                 CPM.pop(0)
         
-        Buzzer.buzzer()
+        Buzzer()
         d1=str(len(CPM))
         d2=str(delta_t)
         data = d1+","+d2+","+str(time())
         print(data)
         mqtt.publish("CPM", d1)
         mqtt.publish("delta_t",d2)
+        
+
