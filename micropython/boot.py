f6fbb2643f8e556440fb20238534735572beec28
Ahnaf Tahmid Chowdhury <ahnaf-tahmid-chowdhury@users.noreply.github.com>
2021-03-12 23:12:58 +0600

    json file will read and saved to config variable

--- micropython/boot.py
+++ micropython/boot.py
@@ -1,5 +1,11 @@
 # This file is executed on every boot (including wake-boot from deepsleep)
 import esp
 esp.osdebug(None)
-#import webrepl
-#webrepl.start()
+
+import gc
+gc.collect()
+
+from json import loads, dumps
+f = open('config.json', 'r')
+config = loads(f.read())
+f.close()
