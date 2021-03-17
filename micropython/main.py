ccefc71e3e2cc0547e7de4884c817ec81a6ad546
Ahnaf Tahmid Chowdhury <ahnaf-tahmid-chowdhury@users.noreply.github.com>
2021-03-17 23:09:37 +0600

    bug fix

--- micropython/main.py
+++ micropython/main.py
@@ -19,9 +19,9 @@ f.close()
 
 def do_connect():
     wlan = network.WLAN(network.STA_IF)
+    led.color(r=700,g=1023)
     wlan.active(True)
     print('connecting to network...')
-    led.color(r=700,g=1023)
     wlan.connect(config['essid'], config['password'])
     sleep(5) # wait 5 sec to connect
     if not wlan.isconnected():
@@ -130,21 +130,25 @@ async def geiger_count():
 
 
 async def machine_freq():
-    global freq
+    global freq, d2, t
     while True:
+        led.color(400, 500)
+        t=ticks_ms()
         if config["powersave"] == "true":
             freq(80000000)
         if config["auto_freq"] == "true":
             freq(auto)
         if config["max_freq"] == "true":
             freq(240000000)
-
+        d2=int(d2+(d2*(ticks_ms()-t)/60000))
+        led.off()
         await uasyncio.sleep_ms(500)
 
 
 async def data_pass():
-    global d2, d3, d4, d5, d6, auto
+    global d2, d3, d4, d5, d6, auto, t
     while True:
+        t=ticks_us()
         d2= int(len(CPM)/2)
         d3 = d2*0.0057
         time = ticks_diff(ticks_ms(),timer)/1000
@@ -167,10 +171,9 @@ async def data_pass():
                 mqtt_local.publish(b"distance",str(d6))
                 mqtt_local.publish(b"temperature",str(d7))
                 mqtt_local.publish(b"freq",str(int(freq()/1000000)))
-                print(d5)
             except:
                 print("local publish failed")
-  
+                
         if d2 < 400:
             await uasyncio.sleep_ms(200)
         elif d2 < 700:
@@ -187,52 +190,67 @@ async def data_pass():
 
 
 async def cloud_publish():
+    global d2, t
     while True:
         if config["cloud"] == "true" :
+            t=ticks_ms()
             try:
+                led.color(700, 700)
                 mqtt_cloud.publish("/v1.6/devices/smartgeiger/total-counts", dumps(d1), qos=0,retain=False)
                 mqtt_cloud.publish("/v1.6/devices/smartgeiger/cpm", dumps(d2), qos=0,retain=False)
                 #added usv as a mathematical formula at ubidots
                 #mqtt_cloud.publish("/v1.6/devices/smartgeiger/usv", dumps(d3), qos=0,retain=False) 
                 mqtt_cloud.publish("/v1.6/devices/smartgeiger/delta-t",dumps(d5), qos=0,retain=False)
                 mqtt_cloud.publish("/v1.6/devices/smartgeiger/temperature",dumps(d7), qos=0,retain=False)
+                led.off()
             except:
-                print("cloud publish faild")        
+                print("cloud publish faild")    
+            d2=int(d2+(d2*(ticks_ms()-t)/60000))
+            
         await uasyncio.sleep(config["time_interval"])
         
 
 async def local_subscribe():
-    global config
+    global config, d2, t
     while True:
-        if config["config_sync"] == "true" and config["local"] == "true":            
+        if config["config_sync"] == "true" and config["local"] == "true":
+            t=ticks_ms()
             try:
                 led.color(700, 700)
                 mqtt_local.check_msg()
+                led.off()
             except:
                 print("local subscribe faild")             
             f = open('config.json', 'w')
             f.write(dumps(config))
             f.close()
+            d2=int(d2+(d2*(ticks_ms()-t)/60000))
         await uasyncio.sleep(config["time_interval"])
         
 
 async def cloud_subscribe():
+    global d2, t
     global config
     while True:
         if config["cloud"] == "true":
+            t=ticks_ms()
             try:
                 led.color(700, 700)
                 mqtt_cloud.subscribe("/v1.6/devices/smartgeiger/buzzer")
                 mqtt_cloud.check_msg()
+                led.off()
             except:
                 print("cloud subscribe faild")
+            d2=int(d2+(d2*(ticks_ms()-t)/60000))
         await uasyncio.sleep(config["time_interval"])
 
 
 async def ifttt():
 
     while True:
+        global d2, t
         if d2 > 100 and config["msg_alert"] == "true":
+            t=ticks_ms()
             try:
                 led.color(700, 700)
                 readings = {'value1':d1, 'value2':d2, 'value3':d3}
@@ -245,10 +263,12 @@ async def ifttt():
                     json=readings,
                     headers=request_headers)
                 request.close()
+                led.off()
                 await uasyncio.sleep(180)
             except OSError as e:
                 print('Failed to read/publish readings.')
                 await uasyncio.sleep(10)
+            d2=int(d2+(d2*(ticks_ms()-t)/60000))
         elif d2>200:
             await uasyncio.sleep(120)
         else:
