dd3852d26303132af8bae1d63eb7088e55aa8b07
Ahnaf Tahmid Chowdhury <ahnaf-tahmid-chowdhury@users.noreply.github.com>
2021-03-28 19:31:44 +0600

    remove d1 and marge with event

--- micropython/main.py
+++ micropython/main.py
@@ -60,7 +60,6 @@ current_tick = ticks_ms()
 CPM = []
 event = 0
 delta_t= 0
-d1= 0
 d2= 0
 auto = 160000000
 
@@ -95,7 +94,7 @@ try:
     mqtt_local.subscribe("smartgiger",qos=0)
     print("waiting for msg...")
     led.color(500, 700)
-    mqtt_local.wait_msg()
+    mqtt_local.check_msg()
     
 except:
     print("local subscribe faild")
@@ -115,7 +114,6 @@ async def geiger_count():
             current_tick = ticks_ms()
             delta_t = ticks_diff(current_tick, last_tick)
             last_tick = current_tick
-            d1=d1+1
 
             if delta_t < 120000:
                 CPM.append(delta_t)
@@ -145,14 +143,14 @@ async def machine_freq():
 
 
 async def data_pass():
-    global d2, d3, d4, d5, d6, auto, t
+    global d2, d3, d4, d5, d6,d7, auto, t
     while True:
         t=ticks_us()
         d2= int(len(CPM)/2)
         d3 = d2*0.0057
         time = ticks_diff(ticks_ms(),timer)/1000
-        avg = d1/time
-        std = (d1**0.5)/time
+        avg = event/time
+        std = (event**0.5)/time
         d4 = str(avg)+ " +/- " +str(std)
         d5 = delta_t
         d6 = usonic.distance_cm()
@@ -162,7 +160,7 @@ async def data_pass():
             
             try:
                 mqtt_local.publish(b"time",str(time))
-                mqtt_local.publish(b"total-counts",str(d1))
+                mqtt_local.publish(b"total-counts",str(event))
                 mqtt_local.publish(b"cpm",str(d2))
                 mqtt_local.publish(b"usv",str(d3))
                 mqtt_local.publish(b"counting-rate",d4)
@@ -193,9 +191,10 @@ async def cloud_publish():
     while True:
         if config["cloud"] == "true" :
             t=ticks_ms()
+
             try:
                 led.color(700, 700)
-                mqtt_cloud.publish("/v1.6/devices/smartgeiger/total-counts", dumps(d1), qos=0,retain=False)
+                mqtt_cloud.publish("/v1.6/devices/smartgeiger/total-counts", dumps(event), qos=0,retain=False)
                 mqtt_cloud.publish("/v1.6/devices/smartgeiger/cpm", dumps(d2), qos=0,retain=False)
                 #added usv as a mathematical formula at ubidots
                 #mqtt_cloud.publish("/v1.6/devices/smartgeiger/usv", dumps(d3), qos=0,retain=False) 
@@ -203,7 +202,7 @@ async def cloud_publish():
                 mqtt_cloud.publish("/v1.6/devices/smartgeiger/temperature",dumps(d7), qos=0,retain=False)
                 led.off()
             except:
-                print("cloud publish faild")    
+                print("cloud publish faild")
             d2=int(d2+(d2*(ticks_ms()-t)/60000))
             
         await uasyncio.sleep(config["time_interval"])
@@ -276,9 +275,9 @@ async def ifttt():
 event_loop = uasyncio.get_event_loop()
 event_loop.create_task(geiger_count())
 event_loop.create_task(data_pass())
+event_loop.create_task(machine_freq())
 event_loop.create_task(cloud_publish())
 event_loop.create_task(local_subscribe())
 event_loop.create_task(cloud_subscribe())
-event_loop.create_task(machine_freq())
 event_loop.create_task(ifttt())
 event_loop.run_forever()
